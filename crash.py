d7bfe240aeddbb6d3db1b2d780980cd4ff401e3d
Stachu1 <stach.basiuk@gmail.com>
2024-12-27 00:56:06 +0100

    Better code structure

--- crash.py
+++ crash.py
@@ -15,48 +15,61 @@ class Color:
 
 
 class Crash:
-    def __init__(self, fee=0):
+    def __init__(self, balance=100, fee=0):
+        self.balance = balance
         self.fee = fee
+        self.bet = None
+    
+    
+    def get_crash_point(self):
+        rnd = random.random()
+        if rnd == 0:
+            return self.get_crash_point()
+        return 1 * (1 - self.fee) / (rnd)
+
+
+    def start_round(self, crash_point):
+        mulimlier = 1
+        while True:
+            print(f"{round(mulimlier, 2)}x", end="\r")
+            
+            mulimlier += 0.01
+            if mulimlier >= crash_point:
+                print(f"{Color.RED}{round(mulimlier, 2)}x{Color.RESET}")
+                break
+            
+            time.sleep(0.1)
+            if select.select([sys.stdin], [], [], 0)[0]:
+                print(f"\x1b[F{Color.GREEN}{round(mulimlier, 2)}x{Color.RESET}")
+                input()
+                self.balance += self.bet * mulimlier
+                break
+    
     
     def run(self):
-        balance = 100
-        
-        while True:
-            try:
-                bet = float(input("\n" + "="*10 + f" Balance: {Color.CYAN}{round(balance, 2)}{Color.RESET} " + "="*10 + "\nBet: "))
-                if bet > balance:
-                    print(f"{Color.RED}Not enough balance{Color.RESET}")
-                    continue
-            except ValueError:
-                print(f"{Color.RED}Invalid bet{Color.RESET}")
-                continue
-            
-            balance -= bet
-            
-            rnd = random.random()
-            crash_point = 1 * (1 - self.fee) / (rnd)
-            
-            mulimlier = 1
+        try:
             while True:
-                print(f"{round(mulimlier, 2)}x", end="\r")
+                try:
+                    self.bet = float(input("\n" + "="*10 + f" Balance: {Color.CYAN}{round(self.balance, 2)}{Color.RESET} " + "="*10 + "\nBet: "))
+                    if self.bet > self.balance:
+                        print(f"{Color.RED}Not enough balance{Color.RESET}")
+                        continue
+                except ValueError:
+                    print(f"{Color.RED}Invalid bet{Color.RESET}")
+                    continue
                 
-                mulimlier += 0.01
-                if mulimlier >= crash_point:
-                    print(f"{Color.RED}{round(mulimlier, 2)}x{Color.RESET}")
-                    break
+                self.balance -= self.bet
+                crash_point = self.get_crash_point()
+                self.start_round(crash_point)
                 
-                time.sleep(0.1)
-                if select.select([sys.stdin], [], [], 0)[0]:
-                    print(f"\x1b[F{Color.GREEN}{round(mulimlier, 2)}x{Color.RESET}")
-                    input()
-                    balance += bet * mulimlier
-                    break
+        except KeyboardInterrupt:
+            print(f"\x1b[G\x1b[2K{Color.YELLOW}Closing...")
+            sys.exit(0)
+            
+            
 
 
 if __name__ == "__main__":
     crash = Crash()
-    try:
-        crash.run()
-    except KeyboardInterrupt:
-        print(f"\x1b[G\x1b[2K{Color.YELLOW}Closing...")
-        sys.exit(0)
+    crash.run()
+    
